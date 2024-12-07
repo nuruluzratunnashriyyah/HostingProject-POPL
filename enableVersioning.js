7acc854a3de45ea2451c4ea0ea392c65f379d6d5
nuruluzratunnashriyyah <165626093+nuruluzratunnashriyyah@users.noreply.github.com>
2024-12-07 23:25:36 +0700

    Create enableVersioning.js

---
+++ enableVersioning.js
@@ -0,0 +1,25 @@
+/**
+ * Enable object versioning for a GCS bucket
+ */
+
+// Uncomment and replace with your bucket name
+// const bucketName = 'your-unique-bucket-name';
+
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+
+async function enableBucketVersioning() {
+  try {
+    await storage.bucket(bucketName).setMetadata({
+      versioning: {
+        enabled: true,
+      },
+    });
+    console.log(`Versioning is enabled for bucket: ${bucketName}`);
+  } catch (error) {
+    console.error(`Failed to enable versioning: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+enableBucketVersioning();
